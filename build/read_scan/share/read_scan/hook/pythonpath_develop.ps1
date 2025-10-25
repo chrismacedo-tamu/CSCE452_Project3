bf2b9e2517379cff63a6976eca83f8574f42e935
Sophie Lamothe <smlamothe@tamu.edu>
2025-10-25 17:28:33 -0500

    add back build and install folders to be tracked

---
+++ build/read_scan/share/read_scan/hook/pythonpath_develop.ps1
@@ -0,0 +1,3 @@
+# generated from colcon_powershell/shell/template/hook_prepend_value.ps1.em
+
+colcon_prepend_unique_value PYTHONPATH "$env:COLCON_CURRENT_PREFIX\/mnt/c/Users/ehtom/OneDrive/Documents/CSCE452/CSCE452_Project3/build/read_scan"
---
+++ build/read_scan/share/read_scan/hook/pythonpath_develop.sh
@@ -0,0 +1,3 @@
+# generated from colcon_core/shell/template/hook_prepend_value.sh.em
+
+_colcon_prepend_unique_value PYTHONPATH "/mnt/c/Users/ehtom/OneDrive/Documents/CSCE452/CSCE452_Project3/build/read_scan"
